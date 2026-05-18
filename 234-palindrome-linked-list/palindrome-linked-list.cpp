e5f9877d92e94cd8dec9265faf59818435e31c09
Syed Naqeeb <naquibsyed992@gmail.com>
2026-05-18 12:06:03 +0530

    Time: 0 ms (100.00%) | Memory: 121.9 MB (43.90%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,18 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+    ListNode*mid(ListNode* head)
+    {
+        ListNode*slow=head;
+        ListNode*fast=head;
+
+        while(fast->next!=NULL && fast->next->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        return slow;
+    }
+  
     bool isPalindrome(ListNode* head) {
-        vector<int> v;
-        
-        while(head != nullptr) {
-            v.push_back(head->val);
-            head = head->next;
+
+        if(head==NULL) return true;
+
+        else if(head->next==NULL) return true;
+
+       
+
+        else
+        {
+
+        ListNode*middle = mid(head);
+
+        ListNode*curr=middle->next;
+        ListNode*prev=NULL;
+        ListNode*next=NULL;
+
+        while(curr!=NULL)
+        {
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+
         }
-        
-        int s = 0, e = v.size() - 1;
-        while(s < e) {
-            if(v[s] != v[e]) return false;
-            s++; e--;
+        middle->next=prev;
+
+        curr =head;
+        next = prev;
+
+        while( next!=NULL)
+        {
+            if(curr->val != next->val) return false;
+            curr=curr->next;
+            next=next->next;
+
+           
         }
+
+        
+
         return true;
+        }
     }
 };
